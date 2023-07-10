f79dce08f7b3acfc5241ebbb6b3f0ce4e2193c52
Petr Stribny <petr@stribny.name>
2023-07-10 20:17:57 +0200

    feat: Add fresh command, update migrations

---
+++ appname/core/management/commands/fresh.py
@@ -0,0 +1,22 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+
+from appname.core.models import User
+
+
+class Command(BaseCommand):
+    help = "(Re)creates a fresh development database"
+
+    def handle(self, *args, **options):
+        management.call_command("reset_db", "--noinput")
+        management.call_command("migrate")
+
+        superuser = User.objects.create(
+            username="superuser",
+            email="superuser@example.com",
+            is_superuser=True,
+            is_staff=True,
+        )
+        superuser.set_password("superuser")
+
+        self.stdout.write(self.style.SUCCESS("Fresh environment is ready!"))
