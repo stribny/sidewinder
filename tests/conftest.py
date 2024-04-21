c4089af1e492323be70a5c62f51a619710d37d91
Petr Stribny <petr@stribny.name>
2024-04-21 21:48:42 +0200

    fix: Add noqa to asterisk import

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from rest_framework.test import APIClient
 
-from .core.fixtures.factories import *
+from .core.fixtures.factories import *  # noqa
 
 os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
 
