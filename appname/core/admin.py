1abb90728fee1fa3a581481183a7a6207c7f66f8
Petr Stribny <petr@stribny.name>
2024-04-21 21:42:03 +0200

    chore: Replace black, isort, flake8 with ruff

--- appname/core/admin.py
+++ appname/core/admin.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 
 from .forms import UserChangeForm, UserCreationForm
